bc199bb53e61d4c37762c87b1aca91057dd18ad1
marvol0 <murithimarvin09@gmail.com>
2024-10-25 23:19:49 +0300

    docs(quick_sort): add documentation

---
+++ divide-and-conquer/quick_sort.py
@@ -0,0 +1,3 @@
+# Quick Sort picks a pivot element, then partitions the array such that elements less than the pivot are on the left, and elements greater are on the right. 
+# This process is applied recursively.
+
