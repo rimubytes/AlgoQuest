e14c82c895ef305b60718eec8421a8e803ede07e
marvol0 <murithimarvin09@gmail.com>
2024-11-13 22:29:47 +0300

    feat: implement merge_two_sorted lists solution
    - implement merge_two_lists function
    - handle cases where on list is empty
    - append remaining nodes from non-empty list

--- easy/merge_two_sorted_lists.py
+++ easy/merge_two_sorted_lists.py
@@ -3,3 +3,37 @@ class ListNode:
         self.val = val
         self.next = next
 
+def merge_two_lists(list1: ListNode, list2: ListNode) -> ListNode:
+    """
+    Merges two sorted linked lists and returns the merged list
+
+    Parameters:
+    list1 (ListNode): The first sorted linked list
+    list2 (ListNode): The second sorted linked list
+
+    Returns:
+    ListNode: The merged sorted linked list
+    """
+
+    # Create a dummy node to serve as the head of the merged list
+    dummy = ListNode()
+    curr = dummy
+
+    # Traverse both lists and append the smaller value to the merged list
+    while list1 and list2:
+        if list1.val <= list2.val:
+            curr.next = list1
+            list1 = list1.next
+        else:
+            curr.next = list2
+            list2 = list2.next
+        curr = curr.next
+
+    # Append the remaining nodes from the non-empty list
+    if list1:
+        curr.next = list1
+    elif list2:
+        curr.next = list2
+    
+    # Return the merged list (excluding the dummy node)
+    return dummy.next
