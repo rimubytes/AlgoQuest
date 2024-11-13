764cea6befecec39e3f2137e62a2e59bad85792b
marvol0 <murithimarvin09@gmail.com>
2024-11-13 22:46:22 +0300

    feat: add ListNode class implementation
    - create ListNode class with value and next pointer

---
+++ easy/reverse_linked_list.py
@@ -0,0 +1,12 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        """
+        Initialize a ListNode with a value and optional next pointer
+
+        Parameters:
+        val (int): The value to be stored in the node
+        next (ListNode): Reference to the next node (default is None)
+        """
+        self.val = val
+        self.next = next
+        
