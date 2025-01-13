b938707064344c7f53f40148d09dd384650b718d
marvol0 <murithimarvin09@gmail.com>
2025-01-13 23:33:36 +0300

    docs(depth): add comprehensive documentation
    - Add function docstrings

--- easy/maximum_depth_of_binary_tree.py
+++ easy/maximum_depth_of_binary_tree.py
@@ -1,5 +1,12 @@
 class TreeNode:
-
+    """
+    Definition for a binary tree node.
+    
+    Attributes:
+        val (int): The value stored in the node
+        left (TreeNode): Reference to the left child node
+        right (TreeNode): Reference to the right child node
+    """
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
