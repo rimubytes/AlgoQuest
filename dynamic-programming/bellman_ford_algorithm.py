069d8061163f39403b172eb9ab579f21651aeb16
marvol0 <murithimarvin09@gmail.com>
2025-01-17 22:51:05 +0300

    docs: add algorithm explanation

--- dynamic-programming/bellman_ford_algorithm.py
+++ dynamic-programming/bellman_ford_algorithm.py
@@ -2,8 +2,19 @@ from typing import List, Dict, Tuple, Optional
 from math import inf
 
 class Graph:
-
+    """
+    A class to represent a weighted directed graph for Bellman-Ford algorithm.
+    
+    Attributes:
+        vertices (int): Number of vertices in the graph
+        edges (List[Tuple[int, int, float]]): List of edges with weights
+    """
     def __init__(self, vertices: int):
-
+        """
+        Initialize graph with given number of vertices.
+        
+        Args:
+            vertices (int): Number of vertices in the graph
+        """
         self.vertices = vertices
         self.edges: List[Tuple[int, int, float]] = []
