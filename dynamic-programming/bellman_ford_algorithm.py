9fda9b587043ed72e1fa84d27cddcfd9ef038864
marvol0 <murithimarvin09@gmail.com>
2025-01-18 20:17:04 +0300

    feat: implement Bellman-Ford core logic

--- dynamic-programming/bellman_ford_algorithm.py
+++ dynamic-programming/bellman_ford_algorithm.py
@@ -29,3 +29,27 @@ class Graph:
             weight (float): Weight of the edge
         """
         self.edges.append((source, dest, weight))
+
+def bellman_ford(graph: Graph, source: int) -> Tuple[Optional[Dict[int, float]], Optional[Dict[int, int]]]:
+
+    # Initialize distances and predecessors (dp table)
+    distances: Dict[int, float] = {i: inf for i in range(graph.vertices)}
+    distances[source] = 0
+    predecessors: Dict[int, int] = {i: None for i in range(graph.vertices)}
+    
+    # Dynamic Programming: Relax edges |V| - 1 times
+    for _ in range(graph.vertices - 1):
+        # For each edge, try to relax it
+        for u, v, weight in graph.edges:
+            # Recurrence relation of DP
+            if distances[u] != inf and distances[u] + weight < distances[v]:
+                distances[v] = distances[u] + weight
+                predecessors[v] = u
+    
+    # Check for negative weight cycles
+    for u, v, weight in graph.edges:
+        if distances[u] != inf and distances[u] + weight < distances[v]:
+            print("Graph contains negative weight cycle")
+            return None, None
+    
+    return distances, predecessors
