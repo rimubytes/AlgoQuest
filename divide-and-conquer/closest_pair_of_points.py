5b7da0d332c7286f92dd031fe4b85200c23da250
marvol0 <murithimarvin09@gmail.com>
2025-01-22 23:13:26 +0300

    docs: document euclidean distance

--- divide-and-conquer/closest_pair_of_points.py
+++ divide-and-conquer/closest_pair_of_points.py
@@ -2,5 +2,5 @@ from typing import List, Tuple
 import math
 
 def distance(p1: Tuple[float, float], p2: Tuple[float, float]) -> float:
-    
+    """Calculate Euclidean distance between two points."""
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
