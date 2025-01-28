e2190b52234ee702777bed3d8b6b38edad8c06bc
marvol0 <murithimarvin09@gmail.com>
2025-01-28 23:05:42 +0300

    chore: ref folder structure

--- divide-and-conquer/quick_sort.py
+++
@@ -1,3 +0,0 @@
-# Quick Sort picks a pivot element, then partitions the array such that elements less than the pivot are on the left, and elements greater are on the right. 
-# This process is applied recursively.
-
