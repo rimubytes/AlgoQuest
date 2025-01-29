3b271126daa5a17a7b2d016e3959ee6ec672ec62
marvol0 <murithimarvin09@gmail.com>
2025-01-29 22:05:14 +0300

    feat(solver): add solve() method implementation

--- backtracking/sudoku-solver.py
+++ backtracking/sudoku-solver.py
@@ -21,3 +21,26 @@ class SudokuSolver:
         self.board = board
         self.SIZE = 9
         self.BOX_SIZE = 3
+
+    def solve(self) -> bool:
+
+        empty = self.find_empty()
+        if not empty:
+            return True
+            
+        row, col = empty
+        
+        for num in range(1, self.SIZE + 1):
+            if self.is_valid(num, row, col):
+                # Try placing this number
+                self.board[row][col] = num
+                
+                # Recursively try to solve the rest
+                if self.solve():
+                    return True
+                
+                # If placing the number didn't lead to a solution,
+                # backtrack by removing it
+                self.board[row][col] = 0
+                
+        return False
