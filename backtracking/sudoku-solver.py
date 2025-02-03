b56b8a656f252f73377480e4385c63bd99e383a9
marvol0 <murithimarvin09@gmail.com>
2025-02-03 20:16:05 +0300

    test: create test cases

--- backtracking/sudoku-solver.py
+++ backtracking/sudoku-solver.py
@@ -113,3 +113,27 @@ class SudokuSolver:
                     print(str(self.board[i][j]) + " ", end="")
 
 def demonstrate_solver():
+    """Demonstrate the Sudoku solver with a sample puzzle."""
+    # Example puzzle (0 represents empty cells)
+    puzzle = [
+        [5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9]
+    ]
+    
+    print("Original Puzzle:")
+    solver = SudokuSolver(puzzle)
+    solver.print_board()
+    
+    print("\nSolving...")
+    if solver.solve():
+        print("\nSolved Puzzle:")
+        solver.print_board()
+    else:
+        print("\nNo solution exists!")
