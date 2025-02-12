1fb1b49290b1270fa24a09fc65dc59fe948990e9
marvol0 <murithimarvin09@gmail.com>
2025-02-12 22:15:13 +0300

    docs: add detailed documentation

--- graph-algorithm/depth_first_search.py
+++ graph-algorithm/depth_first_search.py
@@ -2,7 +2,20 @@ from typing import List, Set, Dict, Optional
 from collections import defaultdict
 
 class Graph:
+    """
+    A class representing a graph using adjacency list representation.
+    
+    Attributes:
+        vertices (int): Number of vertices in the graph
+        graph (Dict[int, List[int]]): Adjacency list representation of the graph
+    """
     
     def __init__(self, vertices: int):
+        """
+        Initialize the graph with given number of vertices.
+        
+        Args:
+            vertices (int): Number of vertices in the graph
+        """
         self.vertices = vertices
         self.graph = defaultdict(list)
