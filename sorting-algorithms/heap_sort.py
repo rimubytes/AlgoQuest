d440f97ea388094963a1a5e0a96577c97bfd391f
marvol0 <murithimarvin09@gmail.com>
2025-03-23 21:58:46 +0300

    feat(algorithms): implements recursive approach for affected subtree reorganization

--- sorting-algorithms/heap_sort.py
+++ sorting-algorithms/heap_sort.py
@@ -11,8 +11,11 @@ def heapify(arr, n, i):
     if right < n and arr[right] > arr[largest]:
         largest = right
     
+    # If largest is not the root, swap and continue heapifying
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]  # Swap
         
+        # Recursively heapify the affected subtree
         heapify(arr, n, largest)
+
 
