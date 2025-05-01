2bb4b7353b90fa7f723f4ae9de4b31b848a79a03
marvol0 <murithimarvin09@gmail.com>
2025-05-01 22:44:49 +0300

    docs(algorithm): add docstrings for dfs function

--- search-algorithms/depth_first_search.py
+++ search-algorithms/depth_first_search.py
@@ -1,4 +1,21 @@
 def dfs(graph, start, visited=None):
+    """
+    Depth-First Search algorithm implementation.
+
+    Parameters:
+    -----------
+    graph : dict
+        A dictionary representing the graph as an adjacency list.
+    start : str or int
+        The starting node for DFS traversal.
+    visited : set
+        A set to keep track of visited nodes. Defaults to None.
+
+    Returns:
+    --------
+    visited : set
+        A set of nodes visited during DFS traversal.
+    """
     if visited is None:
         visited = set()
 
