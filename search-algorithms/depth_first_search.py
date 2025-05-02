e6d664a31565561f152218f30374cbbebd97a2d7
marvol0 <murithimarvin09@gmail.com>
2025-05-02 22:31:02 +0300

    chore(example): add sample graph and run DFS from node A

--- search-algorithms/depth_first_search.py
+++ search-algorithms/depth_first_search.py
@@ -28,3 +28,19 @@ def dfs(graph, start, visited=None):
             dfs(graph, neighbor, visited)
 
     return visited
+
+# Example Usage
+if __name__ == "__main__":
+    
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['D', 'E'],
+        'C': ['F'],
+        'D': [],
+        'E': ['F'],
+        'F': []
+    }
+
+    # Start DFS from node 'A'
+    visited_nodes = dfs(graph, 'A')
+    print("DFS Traversal Order:", visited_nodes)
