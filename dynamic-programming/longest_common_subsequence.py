21706b10bd9ac318092a44ccf697d8ea1d907b4a
marvol0 <murithimarvin09@gmail.com>
2025-05-10 23:41:00 +0300

    feat(lcs): add length-only calculation function

--- dynamic-programming/longest_common_subsequence.py
+++ dynamic-programming/longest_common_subsequence.py
@@ -54,3 +54,22 @@ def longest_common_subsequence(text1, text2):
     
     # Return the reconstructed LCS (reversed, since we built it backwards)
     return ''.join(reversed(lcs))
+
+def lcs_length(text1, text2):
+
+    # Get the lengths of the input strings
+    m, n = len(text1), len(text2)
+    
+    # Create a 2D DP table
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    # Fill the dp table
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    
+    # The bottom-right cell contains the length of the LCS
+    return dp[m][n]
